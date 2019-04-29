48466594e9f5da1c5e2a308137dfb5e1bec1c533
Felix <felix.delrio.1@gmail.com>
2019-04-29 15:00:20 +0200

    $.get v3 res.usuario.user

--- js/login.js
+++ js/login.js
@@ -1,23 +1,23 @@
 // Cuando se cargue la ventana/pagina completamente
-$(window).on("load", function () {
+$(document).on("load", function () {
 
     // Funcionalidad con la API
 
     (localStorage.getItem("tk") != null || localStorage.getItem("tk") != undefined) ? location.href = "login.html" : ""
 
+    var textoUsuario = $("input:eq(0)").val();
+    var textoPass = $("input:eq(1)").val()
+
     $("button:eq(0)").click(function () {
-        var textoUsuario = $("input:eq(0)").val();
-        var textoPass = $("input:eq(1)").val()
 
         if (textoUsuario != "" && textoPass != "") {
 
             var uri = "https://proyecto-mdc-api.herokuapp.com/validarUsuario/" + textoUsuario + "/" + textoPass
             console.log("URI: " + uri)
             $.get(uri, function (res) {
-            }).done(function (res) {
-                if (res.user == textoUsuario) {
-                    console.log("USER: " + res.user)
-                    console.log("PASS: " + res.pass)
+                if (res.usuario.user == textoUsuario) {
+                    console.log("USER:"+res.usuario.user)
+                    console.log("TextoUsuario:"+textoUsuario)
                     localStorage.setItem("tk", textoUsuario);
                     location.href = "inicio.html";
                 } else {
@@ -30,6 +30,5 @@ $(window).on("load", function () {
         }
         return false;
     });
-
 
 });
