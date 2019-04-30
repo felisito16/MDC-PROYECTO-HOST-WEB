d72e8fff6aa256ffdd5f6babe1f89bf1d01e1c88
Felix <felix.delrio.1@gmail.com>
2019-04-30 18:21:12 +0200

    token Inicio.html // Borrado de comentarios UNUSE

--- js/inicio.js
+++ js/inicio.js
@@ -1,36 +1,10 @@
 // Cuando se cargue la ventana/pagina completamente
 $(window).on("load", function () {
-
-    // CSS
+    
+    (localStorage.getItem("abreteSesamo") != null || localStorage.getItem("abreteSesamo") != undefined) ? location.href = "app" : ""
 
     // Por defecto, Inicio activado en el menu
     $("a.aAumentado:contains('Inicio')").addClass("activo").css({ "pointer-events": "none" })
-
-    // Esconder/Mostrar tablas
-    /* var h2Pendientes = false
-    var h2Erroneas = false
-
-    $("h2.h2Registro").click(function () {
-        var h2Texto = $(this).text()
-        var divSelec = 'divTabla' + h2Texto
-        if (h2Texto == "Pendientes") {
-            if (h2Pendientes == true) {
-                $('.' + divSelec).removeClass("hide").show()
-                h2Pendientes = false
-            } else {
-                $('.' + divSelec).addClass("hide").hide()
-                h2Pendientes = true
-            }
-        } else if (h2Texto == "Erróneas") {
-            if (h2Erroneas == true) {
-                $('.divTablaErroneas').removeClass("hide").show()
-                h2Erroneas = false
-            } else {
-                $('.divTablaErroneas').addClass("hide").hide()
-                h2Erroneas = true
-            }
-        }
-    }) */
 
     // Evento Hover Pendientes/Erroneas h2 (titulo)
     $("h2.h2Registro").hover(function () {
--- js/login.js
+++ js/login.js
@@ -10,40 +10,11 @@ $(document).ready(function () {
 
         if ((textoUsuario != "") && (textoPass != "")) {
 
-            /* $.ajax({
-                type: "GET",
-                url: uri,
-                dataType: "json",
-                beforeSend: function () {
-                    //console.log("Opteniendo usuario.");
-                },
-                success: function (res) {
-                    alert(res.usuario[0].user)
-                    alert(res.usuario.user)
-                    alert(res[0].usuario)
-                    alert(res[0].usuario.user)
-                    console.log("USER:" + res.usuario.user)
-                    console.log("PASS:" + res.usuario.pass)
-                    console.log("TextoUsuario:" + textoUsuario)
-                    console.log("TextoPass:" + textoPass)
-                    if (res.usuario.user == textoUsuario) {
-                        localStorage.setItem("tk", res.usuario._id);
-                        location.href = "inicio.html";
-                    } else {
-                        alert("Introduce un usuario y contraseña validos");
-                    }
-                },
-                timeout: 2000
-            });
-        } else {
-            alert("Inserte todos los campos");
-        } */
             var uri = "https://proyecto-mdc-api.herokuapp.com/validarUsuario/" + textoUsuario + "/" + textoPass;
-            /* var uri = "https://proyecto-mdc-api.herokuapp.com/validarUsuario/Felix/123" */
             console.log("URI: " + uri)
             $.get(uri, function (res) {
-                if (res.usuario[0].user) {
-                    localStorage.setItem("tk", res.usuario[0]._id);
+                if (res.usuario[0].user == textoUsuario) {
+                    localStorage.setItem("abreteSesamo", res.usuario[0]._id);
                     location.href = "inicio.html";
                 } else {
                     alert("Introduce un usuario y contraseña validos");
